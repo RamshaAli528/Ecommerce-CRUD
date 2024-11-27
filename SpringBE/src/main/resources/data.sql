278e946236b19bf07857480c79ec608a6b06b22b
Ramsha Ali <124781985+RamshaAli528@users.noreply.github.com>
2024-11-27 10:24:32 +0500

    Add files via upload

---
+++ SpringBE/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Product (name, description, brand, price, category, release_date, product_available, stock_quantity)
+VALUES
+  ('Product 1', 'Description for Product 1', 'Brand A', 100.50, 'Category 1', '2024-11-19', true, 50),
+  ('Product 2', 'Description for Product 2', 'Brand B', 150.75, 'Category 2', '2024-11-19', false, 30),
+  ('Product 3', 'Description for Product 3', 'Brand C', 200.00, 'Category 3', '2024-11-19', true, 70);
